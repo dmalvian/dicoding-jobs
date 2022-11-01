3cf6c3a5d9fe4c53cd0a7fe852d0c9dd592c544c
dmalvian <dmalvian@gmail.com>
2022-11-01 14:28:59 +0700

    change to react icon, complete authed navbar (#3)

--- resources/js/Components/NavBar.jsx
+++ resources/js/Components/NavBar.jsx
@@ -4,6 +4,7 @@ import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link } from '@inertiajs/inertia-react';
+import { FiBell, FiMenu, FiX, FiChevronDown, FiBriefcase } from 'react-icons/fi';
 
 export default function NavBar({ auth }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
@@ -29,85 +30,87 @@ export default function NavBar({ auth }) {
                         </div>
                     </div>
 
-                    <div className="hidden sm:flex sm:items-center sm:ml-6">
-                        <div className="ml-3 relative">
-                            <Dropdown>
-                                <Dropdown.Trigger>
-                                    <span className="inline-flex rounded-md">
-                                        <button
-                                            type="button"
-                                            className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                        >
-                                            {auth.user.name}
-
-                                            <svg
-                                                className="ml-2 -mr-0.5 h-4 w-4"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 20 20"
-                                                fill="currentColor"
-                                            >
-                                                <path
-                                                    fillRule="evenodd"
-                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                    clipRule="evenodd"
-                                                />
-                                            </svg>
-                                        </button>
-                                    </span>
-                                </Dropdown.Trigger>
-
-                                <Dropdown.Content>
-                                    <Dropdown.Link
-                                        href={route("logout")}
-                                        method="post"
-                                        as="button"
-                                    >
-                                        Log Out
-                                    </Dropdown.Link>
-                                </Dropdown.Content>
-                            </Dropdown>
+                    <div className="flex space-x-2">
+                        <div className="hidden sm:flex sm:items-center">
+                            <div className="flex items-center relative left-7">
+                                <FiBriefcase className="text-xl text-blue-500" />
+                            </div>
+                            <select className="text-sm pl-9">
+                                <option>
+                                Siap untuk di-interview
+                                </option>
+                                <option>
+                                Belum siap di-interview
+                                </option>
+                            </select>
                         </div>
-                    </div>
 
-                    <div className="-mr-2 flex items-center sm:hidden">
-                        <button
-                            onClick={() =>
-                                setShowingNavigationDropdown(
-                                    (previousState) => !previousState
-                                )
-                            }
-                            className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-                        >
-                            <svg
-                                className="h-6 w-6"
-                                stroke="currentColor"
-                                fill="none"
-                                viewBox="0 0 24 24"
+                        <div className="flex items-center ml-6">
+                            <div className="ml-3 relative">
+                                <Dropdown>
+                                    <Dropdown.Trigger>
+                                        <span className="inline-flex rounded-md">
+                                            <button
+                                                type="button"
+                                                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                            >
+                                                <div className="rounded-full border-blue-400 border-solid border-2">
+                                                    <img src="/images/profiles/default.png" alt={auth.user.name} />
+                                                </div>
+
+                                                <FiChevronDown className="ml-1" />
+                                            </button>
+                                        </span>
+                                    </Dropdown.Trigger>
+
+                                    <Dropdown.Content>
+                                        <div className="px-4">
+                                            <div className="font-medium text-base text-gray-800">
+                                                {auth.user.name}
+                                            </div>
+                                            <div className="font-medium text-sm text-gray-500">
+                                                {auth.user.email}
+                                            </div>
+                                        </div>
+                                        <div className="mt-3 space-y-1">
+                                            <Dropdown.Link
+                                                href={route("logout")}
+                                                method="post"
+                                                as="button"
+                                            >
+                                                Log Out
+                                            </Dropdown.Link>
+                                        </div>
+                                    </Dropdown.Content>
+                                </Dropdown>
+                            </div>
+
+                            <FiBell className="text-xl ml-2" />
+                        </div>
+
+                        <div className="-mr-2 flex items-center sm:hidden">
+                            <button
+                                onClick={() =>
+                                    setShowingNavigationDropdown(
+                                        (previousState) => !previousState
+                                    )
+                                }
+                                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                             >
-                                <path
+                                <FiMenu
                                     className={
                                         !showingNavigationDropdown
-                                            ? "inline-flex"
-                                            : "hidden"
-                                    }
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M4 6h16M4 12h16M4 18h16"
-                                />
-                                <path
+                                            ? "inline-flex text-xl"
+                                            : "hidden text-xl"
+                                } />
+                                <FiX
                                     className={
                                         showingNavigationDropdown
-                                            ? "inline-flex"
-                                            : "hidden"
-                                    }
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M6 18L18 6M6 6l12 12"
-                                />
-                            </svg>
-                        </button>
+                                            ? "inline-flex text-xl"
+                                            : "hidden text-xl"
+                                } />
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -118,33 +121,25 @@ export default function NavBar({ auth }) {
                 }
             >
                 <div className="pt-2 pb-3 space-y-1">
+                    <div className="flex items-center">
+                        <div className="flex items-center relative left-7">
+                            <FiBriefcase className="text-xl text-blue-500" />
+                        </div>
+                        <select className="text-sm pl-9">
+                            <option>
+                            Siap untuk di-interview
+                            </option>
+                            <option>
+                            Belum siap di-interview
+                            </option>
+                        </select>
+                    </div>
                     <ResponsiveNavLink href="/">
                         Looking for job
                     </ResponsiveNavLink>
                     <ResponsiveNavLink href="/">
                         Hiring
                     </ResponsiveNavLink>
-                </div>
-
-                <div className="pt-4 pb-1 border-t border-gray-200">
-                    <div className="px-4">
-                        <div className="font-medium text-base text-gray-800">
-                            {auth.user.name}
-                        </div>
-                        <div className="font-medium text-sm text-gray-500">
-                            {auth.user.email}
-                        </div>
-                    </div>
-
-                    <div className="mt-3 space-y-1">
-                        <ResponsiveNavLink
-                            method="post"
-                            href={route("logout")}
-                            as="button"
-                        >
-                            Log Out
-                        </ResponsiveNavLink>
-                    </div>
                 </div>
             </div>
         </nav>
